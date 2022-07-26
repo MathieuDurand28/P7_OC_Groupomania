89a37711dcdc5a2e1230e10a1c64119eea438a3c
Mathieu Durand <32414550+MathieuDurand28@users.noreply.github.com>
2022-07-26 15:40:15 +0200

    fake data implement

--- client/src/components/Content/Content.jsx
+++ client/src/components/Content/Content.jsx
@@ -30,6 +30,18 @@ function Content() {
                         <p>Commenter</p>
                     </div>
                 </div>
+                <div className="card">
+                    <div className="card-head">
+                        <p className="font-20">Charles</p>
+                    </div>
+                    <div className="card-content">
+                        <p><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab animi asperiores, cum deserunt doloremque eligendi est laudantium minima nobis optio porro quaerat quidem quod saepe voluptas voluptatum. Aspernatur, inventore?</span><span>Ab adipisci animi aspernatur deleniti dolore, expedita ipsum numquam voluptate voluptates! Aliquid consectetur eaque earum eum excepturi incidunt inventore laboriosam, omnis qui repudiandae sed temporibus veniam. Asperiores dolore est iste!</span></p>
+                    </div>
+                    <div className="card-footer">
+                        <p>👍 2</p>
+                        <p>Commenter</p>
+                    </div>
+                </div>
             </main>
             <aside className="aside"></aside>
         </div>
